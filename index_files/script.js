c0b6ecadf78e401c826e5e79db5fd4829202a5ca
Harsh Khilawala <harsh.khilawala27@gmail.com>
2025-10-13 23:42:23 -0400

    EX-IMP: update HTML Structure

--- index_files/script.js
+++ index_files/script.js
@@ -1,31 +1,59 @@
-// const bachelorDegreeElement = document.querySelector(".bachelor-degree h2");
+let semesterListElements = document.querySelectorAll(".semester-terms-list");
 
-// if (window.matchMedia('screen and (max-width: 320px)').matches) {
-//     console.log(screen.width);
-//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-//     bachelorDegreeElement.style.font = "1rem";
-// }
 
-// if (window.matchMedia('screen and (max-width: 481px)').matches) {
-//     console.log(screen.width);
-//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-//     bachelorDegreeElement.style.font = "1rem";
-// }
 
-// if (window.matchMedia('screen and (max-width: 641px)').matches) {
-//     console.log(screen.width);
-//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-//     bachelorDegreeElement.style.fontSize = "1rem";
-// }
 
-// if (window.matchMedia('screen and (max-width: 961px)').matches) {
-//     console.log(screen.width);
-//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-//     bachelorDegreeElement.style.font = "1rem;";
-// }
 
-// if (window.matchMedia('screen and (max-width: 320px)').matches) {
-//     console.log(screen.width);
-//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-//     bachelorDegreeElement.style.font = "1rem";
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /* smartphones, iPhone, portrait 480x320 phones */
+if (window.matchMedia('screen and (max-width: 320px)').matches) {
+    
+}
+
+// /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */
+if (window.matchMedia('screen and (max-width: 481px)').matches) {
+    
+}
+
+// /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */
+if (window.matchMedia('screen and (max-width: 641px)').matches) {
+    
+}
+
+// /* tablet, landscape iPad, lo-res laptops ands desktops */
+if (window.matchMedia('screen and (max-width: 961px)').matches) {
+    
+}
+
+// /* big landscape tablets, laptops, and desktops */ 
+if (window.matchMedia('screen and (max-width: 1025px)').matches) {
+    
+}
+
+// hi-res laptops and desktops 
+if (window.matchMedia('screen and (max-width: 1281px)').matches) {
+    
+}
+
+
+// For education section in mobile screens
+// Remove bullets
+// Remove margin
+// using .masters-term-list remove all the margiin
+// Same for bachelor
+
+
+// EXTREMELY IMPORTANT - SAVE!!!
+// head, body {header, nav, main, section, footer}
