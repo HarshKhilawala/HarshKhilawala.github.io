52d39387ad86a716661be66f09f8c07b94b6394c
Harsh Khilawala <harsh.khilawala27@gmail.com>
2025-10-13 21:06:39 -0400

    structure education section

--- index_files/script.js
+++ index_files/script.js
@@ -1,31 +1,31 @@
-const bachelorDegreeElement = document.querySelector(".bachelor-degree h2");
+// const bachelorDegreeElement = document.querySelector(".bachelor-degree h2");
 
-if (window.matchMedia('screen and (max-width: 320px)').matches) {
-    console.log(screen.width);
-    bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-    bachelorDegreeElement.style.font = "1rem";
-}
+// if (window.matchMedia('screen and (max-width: 320px)').matches) {
+//     console.log(screen.width);
+//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
+//     bachelorDegreeElement.style.font = "1rem";
+// }
 
-if (window.matchMedia('screen and (max-width: 481px)').matches) {
-    console.log(screen.width);
-    bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-    bachelorDegreeElement.style.font = "1rem";
-}
+// if (window.matchMedia('screen and (max-width: 481px)').matches) {
+//     console.log(screen.width);
+//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
+//     bachelorDegreeElement.style.font = "1rem";
+// }
 
-if (window.matchMedia('screen and (max-width: 641px)').matches) {
-    console.log(screen.width);
-    bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-    bachelorDegreeElement.style.fontSize = "1rem";
-}
+// if (window.matchMedia('screen and (max-width: 641px)').matches) {
+//     console.log(screen.width);
+//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
+//     bachelorDegreeElement.style.fontSize = "1rem";
+// }
 
-if (window.matchMedia('screen and (max-width: 961px)').matches) {
-    console.log(screen.width);
-    bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-    bachelorDegreeElement.style.font = "1rem;";
-}
+// if (window.matchMedia('screen and (max-width: 961px)').matches) {
+//     console.log(screen.width);
+//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
+//     bachelorDegreeElement.style.font = "1rem;";
+// }
 
-if (window.matchMedia('screen and (max-width: 320px)').matches) {
-    console.log(screen.width);
-    bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
-    bachelorDegreeElement.style.font = "1rem";
-}
+// if (window.matchMedia('screen and (max-width: 320px)').matches) {
+//     console.log(screen.width);
+//     bachelorDegreeElement.textContent = "B.Tech. in Computer Science and Engineering";
+//     bachelorDegreeElement.style.font = "1rem";
+// }
